13a239841c5fe6fec0ba69a76e38c3c74cf87de7
meenadaamodharan <51082170+meenadaamodharan@users.noreply.github.com>
2019-05-29 16:24:59 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+mee=int(input())
+alp=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if mee==alp:
+	print("Alphabet")
+else:
+	print("Invalid")
